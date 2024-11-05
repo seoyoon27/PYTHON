cd9c2de5cbf0760242bfb411f5eaa20e1075c53d
seoyoon27 <lamhuyentran27pt@gmail.com>
2024-11-05 22:39:28 +0900

    add d.py

---
+++ a.py.txt/d.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    age = 11
+    la_con_nit = False
+
+    if age < 10:
+        la_con_nit = True
+    if la_con_nit:
+        print("con nit")
+    elif age<18:
+        print("trau tre")
+    else:
+        print("nguoi lon")
